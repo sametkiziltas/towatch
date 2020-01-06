b63c1ae29de83d2db5770be0654f0afa1579160c
samet.kiziltas <samet.kiziltas@MRK-PK-NB-SAKI>
2020-01-06 10:44:22 +0300

    editing first

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-
-
-    
+    <WatchList msg="İşte izleme listen burada"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WatchList from './components/WatchList.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    WatchList
   }
 }
 </script>
